b30b3958fcce1956d1db682076dedb303df9c54a
Vincent REHM <contact@vincentrehm.fr>
2021-01-29 22:15:51 +0100

    Creating Rooms in TDD

---
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -0,0 +1,27 @@
+# test/chatter_web/features/user_creates_new_chat_room_test.exs
+
+defmodule ChatterWeb.UserCreatesNewChatRoomTest do
+    use ChatterWeb.FeatureCase, async: true
+
+    test "user creates a new chat room successfully", %{session: session} do
+        session
+        |> visit(rooms_index())
+        |> click(new_chat_link())
+        |> create_chat_room(name: "elixir")
+        |> assert_has(room_title("elixir"))
+    end
+
+    defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
+
+    defp new_chat_link, do: Query.link("New chat room")
+
+    defp create_chat_room(session, name: name) do
+        session
+        |> fill_in(Query.text_field("Name"), with: name)
+        |> click(Query.button("Submit"))
+    end
+
+    defp room_title(title) do
+        Query.data("role", "room-title", text: title)
+    end
+end
